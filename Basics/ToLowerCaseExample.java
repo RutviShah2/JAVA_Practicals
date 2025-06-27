c4467ef5f12fc91050383f5bb3d45d04c08cde16
Rutvi Shah <shahrutvi020@gmail.com>
2025-06-27 20:56:01 +0530

    Add files via upload

---
+++ Basics/ToLowerCaseExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ToLowerCaseExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String lowercase = input.toLowerCase();
+
+      System.out.println("Lowercase version: " + lowercase);
+    }
+}
