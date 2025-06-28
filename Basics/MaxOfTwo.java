e302b9600c2ef408b06eb8bc0ff4e5971b3dff7b
Rutvi Shah <shahrutvi020@gmail.com>
2025-06-28 09:38:23 +0530

    initial commit

---
+++ Basics/MaxOfTwo.java
@@ -0,0 +1,7 @@
+public class MaxOfTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 25;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum is: " + max);
+    }
+}
