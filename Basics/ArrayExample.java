920fb8b15e35babb6fae9859959892989484aa01
Rutvi Shah <shahrutvi020@gmail.com>
2025-06-28 09:36:01 +0530

    initial commit

---
+++ Basics/ArrayExample.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] marks = {85, 90, 78};
+        for(int i = 0; i < marks.length; i++) {
+            System.out.println("Mark " + (i+1) + ": " + marks[i]);
+        }
+    }
+}
