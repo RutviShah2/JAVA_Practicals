546ced91f758a84442d248e1bc6c9f6039d05dc2
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-02 08:13:17 +0530

    initial commit

---
+++ Patterns/diamond.java
@@ -0,0 +1,23 @@
+public class diamond {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Upper triangle
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+
+        // Lower triangle
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
