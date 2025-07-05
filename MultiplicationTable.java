780e98adf4762d1de94b3ddcaeb10ec2dd79f651
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-05 17:35:56 +0530

    initial commit

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+public public class MultiplicationTable {
+
+    static void printTable(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 9;
+        printTable(number);
+    }
+}
+ {
+    
+}
