c7ee4921e4e427db10c20a5fe4f5e77a06db30f5
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-08 19:07:44 +0530

    initial commit

---
+++ Patterns/SquarePattern.java
@@ -0,0 +1,15 @@
+public public class SquarePattern {
+    public static void main(String[] args) {
+        int size = 5;  // You can change this number
+
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print("*");
+            }
+            System.out.println();  // Move to the next line after each row
+        }
+    }
+}
+ {
+    
+}
