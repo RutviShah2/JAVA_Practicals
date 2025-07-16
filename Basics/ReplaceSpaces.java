555151b9d3ab60dabc1aeb59b512f7ad0966220f
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-16 13:58:30 +0530

    Update ReplaceSpaces.java

--- Basics/ReplaceSpaces.java
+++ Basics/ReplaceSpaces.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
 public class ReplaceSpaces {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
-
-        // Take user input
+        
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
-
-        // Replace spaces with underscores
+        
         String modified = input.replace(' ', '_');
-
-        // Show the result
         System.out.println("Modified string: " + modified);
     }
 }
