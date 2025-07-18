114dbcf9baf05748b40fb6631a81ca0f0c564ef4
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-18 17:02:17 +0530

    Update SquarePattern.java

--- Patterns/SquarePattern.java
+++ Patterns/SquarePattern.java
@@ -1,6 +1,6 @@
 public public class SquarePattern {
     public static void main(String[] args) {
-        int size = 5;  // You can change this number
+        int size = 5;
 
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
