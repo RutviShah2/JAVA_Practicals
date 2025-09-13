802e2b7080b81abb594774f256c4ab5ae73d30f4
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-13 12:10:08 +0530

    initial commit

--- Practicals_Lab/InheritanceInterfacesAndPackages/StudentManagement/Student/Utility/GradeCalculator.java
+++ Practicals_Lab/InheritanceInterfacesAndPackages/StudentManagement/Student/Utility/GradeCalculator.java
@@ -1,1 +1,10 @@
+package StudentManagement.Student.Utility;
 
+public class GradeCalculator {
+    public static String calculateGrade(int marks) {
+        if (marks >= 90) return "A";
+        else if (marks >= 75) return "B";
+        else if (marks >= 50) return "C";
+        else return "F";
+    }
+}
